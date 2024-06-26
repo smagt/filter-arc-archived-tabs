ae9ad7686336f285afd2f43120137a67b3d4f79f
Patrick van der Smagt <smagt@argmax.ai>
2024-06-26 21:25:59 +0200

    v 0.2.0

--- clean-arc.sh
+++ clean-arc.sh
@@ -9,6 +9,9 @@
 
 verbose=0 # Flag to track verbosity
 
+PYTHON=/usr/bin/python3
+JQ=/opt/homebrew/bin/jq
+
 # Function to print messages in verbose mode
 print_verbose() {
     if [ "$verbose" -eq 1 ]; then
@@ -36,49 +39,70 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
+# Check for jq installation
+jq_installed=1
+if ! command -v $JQ &> /dev/null; then
+    echo "Warning: jq is not found. JSON validation will be skipped."
+    jq_installed=0
+fi
+
+
 # Define the input and output file paths
 INPUT_FILE="$HOME/Library/Application Support/Arc/StorableArchiveItems.json"
 OUTPUT_FILE="FilteredStorableArchiveItems.json"
 
-# Check if the input JSON file is valid
-if ! jq empty "$INPUT_FILE" > /dev/null 2>&1; then
-    echo "Error: The JSON file at '$INPUT_FILE' is not valid." >&2
-    exit 1
+# Check if the input JSON file is valid, only if jq is installed
+if [ "$jq_installed" -eq 1 ]; then
+    if ! $JQ empty "$INPUT_FILE" > /dev/null 2>&1; then
+        echo "Error: The JSON file at '$INPUT_FILE' is not valid." >&2
+        exit 1
+    fi
+    print_verbose "Input JSON file is valid."
 fi
-print_verbose "Input JSON file is valid."
 
 # Change directory to the directory where this script resides
 cd "$(dirname "$0")"
 
 # Execute the Python script with the provided arguments and redirect the output
 if [ "$verbose" -eq 1 ]; then
-    python filter.py "$1" "$2" "$3" -v < "$INPUT_FILE" > "$OUTPUT_FILE"
+    $PYTHON filter.py "$1" "$2" "$3" -v < "$INPUT_FILE" > "$OUTPUT_FILE"
 else
-    python filter.py "$1" "$2" "$3" < "$INPUT_FILE" > "$OUTPUT_FILE"
+    $PYTHON filter.py "$1" "$2" "$3" < "$INPUT_FILE" > "$OUTPUT_FILE"
 fi
 
-# Check if the output JSON file is valid
-if ! jq empty "$OUTPUT_FILE" > /dev/null 2>&1; then
-    echo "Error: The generated JSON file at '$OUTPUT_FILE' is not valid." >&2
-    rm "$OUTPUT_FILE" # Remove the invalid output file
-    exit 1
-else
-    print_verbose "Filtering complete. Output saved to '$OUTPUT_FILE'."
-    print_verbose "The generated JSON file is valid."
-    # Before overwriting the original input file with the modified output
-    timestamp=$(date "+.%Y-%m-%d-%H-%M-%S") # Generate timestamp
-    backup_file="${INPUT_FILE%.json}${timestamp}.json" # Append timestamp before .json extension
-
-    mv "$INPUT_FILE" "$backup_file" # Rename original file to include timestamp
-    print_verbose "Original file backed up as '$backup_file'."
-
-    # Maintain at most 20 historic backup files
-    backups=$(ls "${INPUT_FILE%.json}".*.json | sort | head -n -20)
-    if [ ! -z "$backups" ]; then
-        echo "$backups" | xargs rm
-        print_verbose "Removed oldest backups, maintaining at most 20 historic files."
+# Check if the output JSON file is valid, only if jq is installed
+if [ "$jq_installed" -eq 1 ]; then
+    if ! $JQ empty "$OUTPUT_FILE" > /dev/null 2>&1; then
+        echo "Error: The generated JSON file at '$OUTPUT_FILE' is not valid." >&2
+        rm "$OUTPUT_FILE" # Remove the invalid output file
+        exit 1
     fi
-
-    mv "$OUTPUT_FILE" "$INPUT_FILE" # Now safely overwrite the original input file with the modified output
-    print_verbose "Original file has been updated."
 fi
+
+print_verbose "Filtering complete. Output saved to '$OUTPUT_FILE'."
+print_verbose "The generated JSON file is valid."
+# Before overwriting the original input file with the modified output
+timestamp=$(date "+.%Y-%m-%d-%H-%M-%S") # Generate timestamp
+backup_file="${INPUT_FILE%.json}${timestamp}.json" # Append timestamp before .json extension
+
+mv "$INPUT_FILE" "$backup_file" # Rename original file to include timestamp
+print_verbose "Original file backed up as '$backup_file'."
+
+# Maintain at most 20 historic backup files
+# Calculate the total number of lines minus 20
+total_lines=$(ls "${INPUT_FILE%.json}".*.json | wc -l)
+let lines_to_keep=total_lines-20
+
+# Use tail to get all but the last 20 lines, if the calculation is positive
+if [ $lines_to_keep -gt 0 ]; then
+    backups=$(ls "${INPUT_FILE%.json}".*.json | sort | tail -n $lines_to_keep)
+else
+    backups=""
+fi
+if [ ! -z "$backups" ]; then
+    echo "$backups" | xargs rm
+    print_verbose "Removed oldest backups, maintaining at most 20 historic files."
+fi
+
+mv "$OUTPUT_FILE" "$INPUT_FILE" # Now safely overwrite the original input file with the modified output
+print_verbose "Original file has been updated."
